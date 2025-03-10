f33713d02f459d32bbb06ce560a6483bed65c426
morteza-mortezai <mrtz.mrtzi.1989@gmail.com-at-003669155861>
2025-03-10 16:35:26 +0330

    get list after deletaion

--- src/app/dashboard/media-list/partials/medialist.action.ts
+++ src/app/dashboard/media-list/partials/medialist.action.ts
@@ -1,26 +1,24 @@
-'use server'
+"use server";
 import { getUserMedia } from "@/src/lib/service/media/getUserMedia";
 import { deleteMedia } from "@/src/lib/service/media/deleteMedia";
 import { IPaginationParams } from "@/src/lib/service/type/paginationParams";
+import { revalidatePath } from "next/cache";
 
-export async function getUserMediaAction(params?:IPaginationParams){
-
-    try {
-        const list=await getUserMedia(params)
-        return list
-    } catch (error) {
-
-        throw new Error('Failed to fetch media.'); 
-    }
+export async function getUserMediaAction(params?: IPaginationParams) {
+  try {
+    const list = await getUserMedia(params);
+    return list;
+  } catch (error) {
+    throw new Error("Failed to fetch media.");
+  }
 }
 
-export async function deleteUserMediaAction( id: number){
-
-    try {
-       await deleteMedia(+id)
-       return { message: 'Media deleted successfully' };
-    } catch (error) {
-        console.log('err',error)
-        throw new Error('Failed to delete media'); 
-    }
+export async function deleteUserMediaAction(id: number) {
+  try {
+    await deleteMedia(+id);
+    revalidatePath("/dashboard/media-list");
+  } catch (error) {
+    console.log("err", error);
+    throw new Error("Failed to delete media");
+  }
 }
